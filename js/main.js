bd084ce0a14ca264efab049207846ae54033a048
daniele-pipino <Danielemacpipino@icloud.com>
2021-07-13 16:10:32 +0200

    Es pari dispari recupero scelte utente

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto. */
+
+// 1-  richiesta informazioni utente
+// 2-  generare il numero random per l'uente e il computer
+// 3-  sommare e dichiarare il vincitore
+
+// ----------------------------------------------
+
+// recupero scelte utente
+
+var evenOdd = prompt('Pari o Dispari?', 'Pari');
+console.log(evenOdd);
+
+var userNumber = parseInt(prompt('Scegli un numero da 1 a 5', '3'));
+console.log(userNumber);
+
+// generazione casuale numero pc
+
+function randomNumber() {
+
+
+}
+
+
+
+
+
+
